b84ecb0d09dd04bd175c7965535dd3dc4e7b8e01
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2017-01-04 21:44:23 +0200

    Fix test

--- spec/purchase_controller_spec.rb
+++ spec/purchase_controller_spec.rb
@@ -5,6 +5,6 @@ describe 'Purchase Controller behavior' do
   it 'should do not be valid to purchase an unavailable plant' do
     p = build(:plant)
     build(:purchase_order, :start_date => 1.days.ago, :end_date => 2.days.from_now, :plant_id => p.id)
-    expect(PurchaseController.check_plant_is_available(p.id, 1.days.from_now)).to be(false)
+    expect(PurchaseOrderController.check_plant_is_available(p.id, 1.days.from_now)).to be(false)
   end
 end
