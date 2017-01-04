fd3098a9d5126ff175a1472858615b0f0bbec424
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2017-01-04 21:30:45 +0200

    Controller test implemented

--- spec/purchase_controller_spec.rb
+++ spec/purchase_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe 'Purchase Controller behavior' do
 
   it 'should do not be valid to purchase an unavailable plant' do
-    pending
+    p = build(:plant)
+    build(:purchase_order, :start_date => 1.days.ago, :end_date => 2.days.from_now, :plant_id => p.id)
+    expect(PurchaseController.check_plant_is_available(1.days.from_now)).to be(false)
   end
 end
